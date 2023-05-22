2c646cfd97df2b7878ce5c96287e03f5f18840fd
Omar Hefnawi <omarhefnawi@gmail.com>
2023-05-21 20:24:21 -0700

    Cleaning up these files that don't do anything

--- Source/Dungeon/DungeonConstants.cpp
+++
@@ -1,9 +0,0 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "DungeonConstants.h"
-
-// FVector TilePositionToWorldPoint(const FIntPoint& point)
-// {
-//   return FVector(point) * TILE_POINT_SCALE;
-// }
